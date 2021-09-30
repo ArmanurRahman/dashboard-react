d4d1bcf82c24daaa60f2f523ee7915a82bccbf6c
Armanur Rahman <mubeenarman19@gmail.com>
2021-09-30 22:50:05 +0900

    implementation of blog

---
+++ src/component/Blog/Blog.jsx
@@ -0,0 +1,221 @@
+import ProfilePic from "../../assets/img/mubeen.jpeg";
+
+const Blog = () => {
+    return (
+        <div className='md:flex space-y-5 md:space-y-0 md:space-x-5 lg:md:space-x-10 mt-4 lg:mt-10'>
+            <div className=' w-full lg:w-1/2 bg-white shadow-lg border border-gray-100 rounded-xl p-5 space-y-5'>
+                <div className='flex items-center'>
+                    <div className='w-10 h-10 rounded-full overflow-hidden mr-3'>
+                        <img
+                            className='w-10 h-10 object-cover'
+                            src={ProfilePic}
+                        />
+                    </div>
+                    <div>
+                        <span className='block leading-3'>Armanur Rahman</span>
+                        <span className='text-gray-400 text-xs'>
+                            Software Engineer
+                        </span>
+                    </div>
+                </div>
+                <h3 className='text-lg font-semibold'>
+                    <a className='hover:text-blue-800' href='#'>
+                        How to properly manage personal budget?
+                    </a>
+                </h3>
+                <div className='flex items-center space-x-7'>
+                    <div>
+                        <div className='flex items-center space-x-2'>
+                            <div className='w-6 text-blue-800'>
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    className='h-5 w-5'
+                                    viewBox='0 0 20 20'
+                                    fill='currentColor'
+                                >
+                                    <path
+                                        fillRule='evenodd'
+                                        d='M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm3 2h6v4H7V5zm8 8v2h1v-2h-1zm-2-2H7v4h6v-4zm2 0h1V9h-1v2zm1-4V5h-1v2h1zM5 5v2H4V5h1zm0 4H4v2h1V9zm-1 4h1v2H4v-2z'
+                                        clipRule='evenodd'
+                                    />
+                                </svg>
+                            </div>
+                            <span className='text-sm text-gray-600'>Video</span>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='flex items-center space-x-2'>
+                            <div className='w-6 text-blue-300'>
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    className='h-5 w-5'
+                                    viewBox='0 0 20 20'
+                                    fill='currentColor'
+                                >
+                                    <path
+                                        fillRule='evenodd'
+                                        d='M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z'
+                                        clipRule='evenodd'
+                                    />
+                                </svg>
+                            </div>
+                            <span className='text-sm text-gray-600'>
+                                15 min
+                            </span>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='flex items-center space-x-2'>
+                            <div className='w-6 text-red-600'>
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    className='h-5 w-5'
+                                    viewBox='0 0 20 20'
+                                    fill='currentColor'
+                                >
+                                    <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+                                </svg>
+                            </div>
+                            <span className='text-sm text-gray-600'>
+                                122 likes
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex items-center justify-between'>
+                    <div className='text-gray-600 font-semibold'>
+                        5 days ago
+                    </div>
+                    <a
+                        href='#'
+                        className='bg-blue-800 text-white rounded-full hover:bg-red-500 px-4 py-3 flex space-x-3 items-center'
+                    >
+                        <span>Connect</span>
+                        <div className='w-4 h-4'>
+                            <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                className='h-5 w-5'
+                                viewBox='0 0 20 20'
+                                fill='currentColor'
+                            >
+                                <path
+                                    fillRule='evenodd'
+                                    d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
+                                    clipRule='evenodd'
+                                />
+                            </svg>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div className=' w-full lg:w-1/2 bg-white shadow-lg border border-gray-100 rounded-xl p-5 space-y-5'>
+                <div className='flex items-center'>
+                    <div className='w-10 h-10 rounded-full overflow-hidden mr-3'>
+                        <img
+                            className='w-10 h-10 object-cover'
+                            src={ProfilePic}
+                        />
+                    </div>
+                    <div>
+                        <span className='block leading-3'>Armanur Rahman</span>
+                        <span className='text-gray-400 text-xs'>
+                            Software Engineer
+                        </span>
+                    </div>
+                </div>
+                <h3 className='text-lg font-semibold'>
+                    <a className='hover:text-blue-800' href='#'>
+                        How to properly manage personal budget?
+                    </a>
+                </h3>
+                <div className='flex items-center space-x-7'>
+                    <div>
+                        <div className='flex items-center space-x-2'>
+                            <div className='w-6 text-blue-800'>
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    className='h-5 w-5'
+                                    viewBox='0 0 20 20'
+                                    fill='currentColor'
+                                >
+                                    <path
+                                        fillRule='evenodd'
+                                        d='M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm3 2h6v4H7V5zm8 8v2h1v-2h-1zm-2-2H7v4h6v-4zm2 0h1V9h-1v2zm1-4V5h-1v2h1zM5 5v2H4V5h1zm0 4H4v2h1V9zm-1 4h1v2H4v-2z'
+                                        clipRule='evenodd'
+                                    />
+                                </svg>
+                            </div>
+                            <span className='text-sm text-gray-600'>Video</span>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='flex items-center space-x-2'>
+                            <div className='w-6 text-blue-300'>
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    className='h-5 w-5'
+                                    viewBox='0 0 20 20'
+                                    fill='currentColor'
+                                >
+                                    <path
+                                        fillRule='evenodd'
+                                        d='M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z'
+                                        clipRule='evenodd'
+                                    />
+                                </svg>
+                            </div>
+                            <span className='text-sm text-gray-600'>
+                                15 min
+                            </span>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='flex items-center space-x-2'>
+                            <div className='w-6 text-red-600'>
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    className='h-5 w-5'
+                                    viewBox='0 0 20 20'
+                                    fill='currentColor'
+                                >
+                                    <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+                                </svg>
+                            </div>
+                            <span className='text-sm text-gray-600'>
+                                122 likes
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex items-center justify-between'>
+                    <div className='text-gray-600 font-semibold'>
+                        5 days ago
+                    </div>
+                    <a
+                        href='#'
+                        className='bg-blue-800 text-white rounded-full hover:bg-red-500 px-4 py-3 flex space-x-3 items-center'
+                    >
+                        <span>Connect</span>
+                        <div className='w-4 h-4'>
+                            <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                className='h-5 w-5'
+                                viewBox='0 0 20 20'
+                                fill='currentColor'
+                            >
+                                <path
+                                    fillRule='evenodd'
+                                    d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
+                                    clipRule='evenodd'
+                                />
+                            </svg>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Blog;
--- src/container/Dashboard/Dashboard.jsx
+++ src/container/Dashboard/Dashboard.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
+import Blog from "../../component/Blog/Blog";
 import MainNavigation from "../../component/MainNavigation/MainNavigation";
 import Tools from "../../component/Tools/Tools";
 
 const Dashboard = () => {
     const [showSidebar, setShowSidebar] = useState(false);
     return (
-        <div className='flex h-full'>
+        <div className='flex'>
             <div className='fixed lg:static bottom-0 w-full lg:w-24 lg:h-full z-10'>
-                <div className=' relative w-full lg:w-24 h-full bg-blue-800'>
+                <div className=' relative lg:fixed w-full lg:w-24 h-full bg-blue-800'>
                     <a
                         href='#'
                         className='hidden absolute top-10 left-1/2 transform -translate-x-1/2 w-14 h-14 lg:flex lg:items-center lg:justify-center text-white rounded-xl hover:bg-white hover:bg-opacity-60'
@@ -45,7 +46,7 @@ const Dashboard = () => {
                     </button>
                 </div>
 
-                <div className='overflow-hidden'>
+                <div className='overflow-hidden lg:overflow-visible pb-14 lg:pb-0'>
                     <div
                         className={`transform lg:transform-none transition-all duration-300 ${
                             showSidebar && "-translate-x-full"
@@ -54,16 +55,94 @@ const Dashboard = () => {
                         <div className='flex lg:space-x-10 mt-4 lg:mt-10'>
                             <div className=' w-full lg:w-3/4 flex-shrink-0 lg:flex-shrink  rounded-xl'>
                                 <div className='flex space-x-4 lg:space-x-10'>
-                                    <div className=' bg-gray-300 rounded-xl h-28 w-full'></div>
-                                    <div className=' bg-gray-300 rounded-xl h-28 w-full'></div>
-                                    <div className=' bg-gray-300 rounded-xl h-28 w-full'></div>
+                                    <div className=' flex flex-col lg:flex-row items-center w-1/3 bg-white shadow-lg border border-gray-100 rounded-xl px-2 py-5 lg:p-5 space-x-0 lg:space-x-5'>
+                                        <div className='bg-indigo-50 p-2 rounded-lg text-blue-800 w-full lg:w-auto'>
+                                            <svg
+                                                xmlns='http://www.w3.org/2000/svg'
+                                                className='w-8 mx-auto'
+                                                fill='none'
+                                                viewBox='0 0 24 24'
+                                                stroke='currentColor'
+                                            >
+                                                <path
+                                                    strokeLinecap='round'
+                                                    strokeLinejoin='round'
+                                                    strokeWidth={2}
+                                                    d='M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z'
+                                                />
+                                            </svg>
+                                        </div>
+                                        <div>
+                                            <span className='block uppercase text-xs text-gray-400 tracking-wider font-semibold'>
+                                                Visitors
+                                            </span>
+                                            <span className=' text-xl lg:text-2xl font-semibold'>
+                                                16025
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div className=' flex flex-col lg:flex-row items-center w-1/3 bg-white shadow-lg border border-gray-100 rounded-xl px-2 py-5 lg:p-5 space-x-0 lg:space-x-5'>
+                                        <div className='bg-indigo-50 p-2 rounded-lg text-blue-800  w-full lg:w-auto'>
+                                            <svg
+                                                xmlns='http://www.w3.org/2000/svg'
+                                                className='w-8 mx-auto'
+                                                fill='none'
+                                                viewBox='0 0 24 24'
+                                                stroke='currentColor'
+                                            >
+                                                <path
+                                                    strokeLinecap='round'
+                                                    strokeLinejoin='round'
+                                                    strokeWidth={2}
+                                                    d='M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z'
+                                                />
+                                                <path
+                                                    strokeLinecap='round'
+                                                    strokeLinejoin='round'
+                                                    strokeWidth={2}
+                                                    d='M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z'
+                                                />
+                                            </svg>
+                                        </div>
+                                        <div>
+                                            <span className='block uppercase text-xs text-gray-400 tracking-wider font-semibold'>
+                                                Customers
+                                            </span>
+                                            <span className=' text-xl lg:text-2xl font-semibold'>
+                                                16025
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div className=' flex flex-col lg:flex-row items-center w-1/3 bg-red-500 shadow-lg border border-red-500 rounded-xl px-2 py-5 lg:p-5  space-x-0 lg:space-x-5'>
+                                        <div className=' bg-red-700 p-2 rounded-lg text-white  w-full lg:w-auto'>
+                                            <svg
+                                                xmlns='http://www.w3.org/2000/svg'
+                                                className='w-8 mx-auto'
+                                                fill='none'
+                                                viewBox='0 0 24 24'
+                                                stroke='currentColor'
+                                            >
+                                                <path
+                                                    strokeLinecap='round'
+                                                    strokeLinejoin='round'
+                                                    strokeWidth={2}
+                                                    d='M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z'
+                                                />
+                                            </svg>
+                                        </div>
+                                        <div>
+                                            <span className='block uppercase text-xs text-red-200 tracking-wider font-semibold'>
+                                                Orders
+                                            </span>
+                                            <span className=' text-xl lg:text-2xl font-semibold text-white'>
+                                                1250
+                                            </span>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div className='bg-gray-300 h-28 w-full mt-4 lg:mt-10 rounded-xl'></div>
 
-                                <div className='flex space-x-4 lg:space-x-10 mt-4 lg:mt-10'>
-                                    <div className='bg-gray-300 rounded-xl h-28 w-full'></div>
-                                    <div className='bg-gray-300 rounded-xl h-28 w-full'></div>
-                                </div>
+                                <Blog />
                             </div>
 
                             <div className=' w-full lg:w-1/4 h-52 flex-shrink-0 lg:flex-shrink bg-gray-300 rounded-xl'>
